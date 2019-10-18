8df0162bfcfdc1ec5af43a435430c8e8ccbc29c3
Tea <tea.desouza@gmail.com>
2019-10-18 20:27:26 +0800

    Add sentinel motion script

---
+++ Assets/Sentinel/Sentinel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Vector3;
+using static UnityEngine.Mathf;
+
+public class Sentinel : MonoBehaviour{
+
+    public float speed = 0.1f;
+    Vector3? target;
+
+    public void Move(Vector3 dir)
+    => target = transform.position + dir;
+
+    void Update(){ if(target.HasValue) UpdatePosition(); }
+
+    void UpdatePosition(){
+        var u = (target.Value - transform.position).normalized;
+        var s = Time.deltaTime * speed;
+        if(s < Distance(target.Value, transform.position)){
+            transform.position += u * s;
+        }else{
+            transform.position = target.Value;
+            target = null;
+        }
+    }
+
+}
